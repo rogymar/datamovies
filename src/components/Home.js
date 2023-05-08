8e60e696d80e5a9475ee532722d821dc669977a2
rogymar <rogymarchirinos@gmail.com>
2023-05-08 12:03:46 -0300

    Agregando el estilo a la cuadricula de peliculas populares debajo debajo del Banner principal en la pagina de inicio.

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 // Components
 import Bannerimage from './BannerImage';
 import Grid from './Grid';
+import Thumb from './Thumb';
 
 // Hooks
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -28,7 +29,16 @@ const Home = () => {
             ) : null}
             <Grid>
                 {state.results.map(movie => (
-                    <div key={movie.id}>{movie.title}</div>
+                    <Thumb
+                        key={movie.id}
+                        clickable
+                        image={
+                            movie.poster_path
+                                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                                : NoImage
+                        }
+                        movieId={movie.id}
+                    />
                 ))}
             </Grid>
         </>
