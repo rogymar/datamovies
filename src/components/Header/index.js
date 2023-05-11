8daff490a61e346ff96a3cf333a972d41098b004
rogymar <rogymarchirinos@gmail.com>
2023-05-10 22:01:55 -0300

    Agregando enrutamientos para: determinar si se puede hacer click en una miniatura y de alli transladarse a la pagina de la pelicula, el errutamiento para integrar lo anterior a la pagina de inicio y poder, al hacer click en el logo de la app, volver a la pagina de inicio, asimismo, el enrutamiento hacia un posible 'Not Found' en algun detalle de alguna pelicula. Se instalaron ademas las dependencias de del enrutador de React para que los enrutamientos funcionaran correctamente.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import RMDBLogo from '../../images/datamovie-logo.png';
 import TMDBLogo from '../../images/tmdb_logo.svg';
@@ -8,7 +9,9 @@ import { Wrapper, Content, LogoImg, TMBDLogoImg } from './Header.styles';
 const Header = () => (
     <Wrapper>
         <Content>
+            <Link to='/'>
             <LogoImg src={RMDBLogo} alt='rmdb-logo' />
+            </Link>
             <TMBDLogoImg src={TMDBLogo} alt='tmdb_logo' />
         </Content>
     </Wrapper>
