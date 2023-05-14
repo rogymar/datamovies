7a051226c326c1008501e94477d8ee958eaee550
rogymar <rogymarchirinos@gmail.com>
2023-05-14 13:49:47 -0300

    Implementando la logica para el almacenamiento por sesion.

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'react';
 // API
 import API from '../API';
 
+// Helpers
+import { isPersistedState } from '../helpers';
+
 const inicitialState = {
     page: 0,
     results: [],
@@ -39,6 +42,15 @@ export const useHomeFetch = () => {
 
     // Initian and Search
     useEffect(() => {
+        if (!searchTerm) {
+            const sessionState = isPersistedState('homeState');
+
+            if (sessionState) {
+                setState(sessionState);
+                return;
+            }
+        }
+
         fetchMovies(1, searchTerm);
     }, [searchTerm]);
 
@@ -49,6 +61,11 @@ export const useHomeFetch = () => {
         fetchMovies(state.page + 1, searchTerm);
         setIsLoadingMore(false);
     }, [isLoadingMore, searchTerm, state.page]);
+
+    // Write to sessionStorage
+    useEffect(() => {
+        if (!searchTerm) sessionStorage.setItem('homeState', JSON.stringify(state))
+    }, [searchTerm, state]);
 
     return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
 };
